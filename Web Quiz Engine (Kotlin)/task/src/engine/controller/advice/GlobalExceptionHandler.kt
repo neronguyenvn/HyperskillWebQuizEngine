8feff40e62083c4362d7db1ca83b998d95fb887c
neronguyenvn <neronguyenvn@gmail.com>
2025-05-10 00:04:25 +0700

    feat: handle for testing /actuator/shutdown separately in GlobalExceptionHandler

--- Web Quiz Engine (Kotlin)/task/src/engine/controller/advice/GlobalExceptionHandler.kt
+++ Web Quiz Engine (Kotlin)/task/src/engine/controller/advice/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package engine.controller.advice
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.ServletWebRequest
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.resource.NoResourceFoundException
 
@@ -14,6 +15,13 @@ class GlobalExceptionHandler {
         e: Exception,
         request: WebRequest
     ): ResponseEntity<Any> {
-        return ResponseEntity.ok().build()
+        val servletRequest = (request as? ServletWebRequest)?.request
+        val uri = servletRequest?.requestURI
+
+        return if (uri == "/actuator/shutdown") {
+            ResponseEntity.ok().build() // Only return OK for this endpoint
+        } else {
+            ResponseEntity.notFound().build()
+        }
     }
 }
